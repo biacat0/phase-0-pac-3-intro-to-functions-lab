91efdf21ae265cdbb57d3fcfe14060fe461141d3
June Biadog <jb.biadog@gmail.com>
2022-08-13 20:00:18 -0400

    commit tree 458977e1879103743082846855cc587dee1a1d88

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    //todo
+}function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+  }function whisper(string) {
+    return string;
+  }
+  "hello!".toLowerCase(); //'hello"
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  //
+  let logShout = "Hello!";
+  console.log(logShout);
+  console.log(logShout.toUpperCase())
+  console.log(logShout === logShout.toUpperCase())
